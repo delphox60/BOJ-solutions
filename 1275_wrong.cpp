011850935e2c72a6a2f5bd103250e71955586261
Gibeom Lim <delphox60@gmail.com>
2021-12-19 17:39:24 +0900

    Update 1275_wrong.cpp

--- 1275_wrong.cpp
+++ 1275_wrong.cpp
@@ -2,22 +2,22 @@
 
 using namespace std;
 
-int N;
-int Q;
-int arr[100005];
-int tree[400005];
+long long N;
+long long Q;
+long long arr[100005];
+long long tree[400005];
 
-int init(int s, int e, int n)
+long long init(long long s, long long e, long long n)
 {
     if (s == e)
         return tree[n] = arr[s];
 
-    int m = (s + e) / 2;
+    long long m = (s + e) / 2;
 
     return tree[n] = init(s, m, 2 * n) + init(m + 1, e, 2 * n + 1);
 }
 
-int sum(int s, int e, int l, int r, int n)
+long long sum(long long s, long long e, long long l, long long r, long long n)
 {
     if (s > r || e < l)
         return 0;
@@ -25,12 +25,12 @@ int sum(int s, int e, int l, int r, int n)
     if (l >= s && r <= e)
         return tree[n];
 
-    int m = (l + r) / 2;
+    long long m = (l + r) / 2;
 
     return sum(s, e, l, m, 2 * n) + sum(s, e, m + 1, r, 2 * n + 1);
 }
 
-void update(int s, int e, int n, int idx, int diff)
+void update(long long s, long long e, long long n, long long idx, long long diff)
 {
     if (idx < s || idx > e)
         return;
@@ -40,7 +40,7 @@ void update(int s, int e, int n, int idx, int diff)
     if (s == e)
         return;
 
-    int m = (s + e) / 2;
+    long long m = (s + e) / 2;
 
     update(s, m, 2 * n, idx, diff);
     update(m + 1, e, 2 * n + 1, idx, diff);
@@ -53,17 +53,17 @@ int main()
 
     cin >> N >> Q;
 
-    for (int i = 1; i <= N; i++)
+    for (long long i = 1; i <= N; i++)
         cin >> arr[i];
 
     init(1, N, 1);
 
-    for (int i = 1; i <= Q; i++)
+    for (long long i = 1; i <= Q; i++)
     {
-        int x, y, a, b;
+        long long x, y, a, b;
         cin >> x >> y >> a >> b;
         cout << sum(x <= y ? x : y, x <= y ? y : x, 1, N, 1) << '\n';
-        int diff = b - arr[a];
+        long long diff = b - arr[a];
         arr[a] += diff;
         update(1, N, 1, a, diff);
     }
