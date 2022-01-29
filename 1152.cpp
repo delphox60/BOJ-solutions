623ffcc6195257997a0da0394893c9c0dcd8c855
Gibeom Lim <delphox60@gmail.com>
2022-01-29 16:00:06 +0900

    Create 1152.cpp

---
+++ 1152.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string a;
+    int count = 0;
+    while (cin >> a)
+        count++;
+
+    cout << count << endl;
+}
