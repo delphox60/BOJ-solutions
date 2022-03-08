2dbbdc7085bb655f84111263cbdcbae98bee6c90
Gibeom Lim <delphox60@gmail.com>
2022-03-08 11:31:31 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+            cout << '*';
+        cout << '\n';
+    }
+    for (int i = n; i > 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+            cout << '*';
+        cout << '\n';
+    }
+}
